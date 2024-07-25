e8941d6668592971e79f19452a0da44e9f5574ec
Remy Liu <renmingl@andreew.cmu.edu>
2024-07-25 11:47:49 -0400

    upload nox and project config files

---
+++ noxfile.py
@@ -0,0 +1,20 @@
+import nox
+
+
+@nox.session
+def flake8(session):
+    session.install(
+        "flake8",
+        "flake8-absolute-import",
+        "flake8-bugbear",
+        "flake8-builtins",
+        "flake8-colors",
+        "flake8-commas",
+        "flake8-comprehensions",
+        "flake8-docstrings",
+        "flake8-import-order",
+        "flake8-pyproject",
+        "flake8-use-fstring",
+        "pep8-naming",
+    )
+    session.run("flake8", "Heimdall/")
