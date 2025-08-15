f055b9d8ce3529a81ae96f4519d4d6b1d94be074
Shahul Alam <alam.shahul@gmail.com>
2025-08-15 17:44:50 -0400

    Fix `tests/test_config.py`.
    Update default `config.yaml`.

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,7 +4,7 @@ from omegaconf import OmegaConf
 
 def test_config_with_overrides():
     with initialize(version_base=None, config_path="../config", job_name="test"):
-        cfg = compose(config_name="config", overrides=["fg=identity", "fe=binning"])
+        cfg = compose(config_name="config", overrides=["fg=identity", "fe=binning_scgpt"])
         resolved_config = OmegaConf.create(OmegaConf.to_yaml(cfg, resolve=True))
 
     # assert resolved_config.fe.args.embedding_parameters.args.out_features == resolved_config.fe.args.d_embedding
