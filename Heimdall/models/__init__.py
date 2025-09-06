3a39dca96400f13277432b0ee11d37643a036e3b
alam-shahul <alam.shahul@gmail.com>
2025-09-06 17:04:00 -0400

    Cell PLM/GenePT model (#131)
    * Refactor `Heimdall/models` interface.
    
    Remove redundant `SinusoidalPositionalEncoding`.
    
    * Style fix.
    
    * Refactor `_models.py`.
    
    Abstract `MaskedAverage` and `Transformer` with parent class.
    
    * Remove redundant comments.
    
    * Fix implementation of `ExpressionWeightedSum`.
    
    * Add test for `ExpressionWeightedSum`.
    
    * Clean up commented out `_models.py` code.
    
    * Rename `MaskedAverage` to `Average`.
    
    Add test for `Average`.

---
+++ Heimdall/models/__init__.py
@@ -0,0 +1,19 @@
+from Heimdall.models._head import ExpressionOnlyCellPredHead, LinearCellPredHead, LinearSeqPredHead, TransformerOutput
+from Heimdall.models._models import Average, ExpressionOnly, ExpressionWeightedSum, HeimdallModel, Transformer
+from Heimdall.models._reducers import AsymmetricConcatReducer, MeanReducer, SumReducer, SymmetricConcatReducer
+
+__all__ = [
+    HeimdallModel.__name__,
+    Transformer.__name__,
+    Average.__name__,
+    ExpressionWeightedSum.__name__,
+    ExpressionOnly.__name__,
+    TransformerOutput.__name__,
+    LinearCellPredHead.__name__,
+    LinearSeqPredHead.__name__,
+    ExpressionOnlyCellPredHead.__name__,
+    SumReducer.__name__,
+    MeanReducer.__name__,
+    SymmetricConcatReducer.__name__,
+    AsymmetricConcatReducer.__name__,
+]
