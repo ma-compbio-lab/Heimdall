984ff2e7ee5bcf54a67a7b9106116db06496bb60
Shahul Alam <alam.shahul@gmail.com>
2025-09-24 14:53:56 -0400

    Move configs.

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import hydra
 from Heimdall.trainer import setup_trainer
 
 
-@hydra.main(config_path="config", config_name="config", version_base="1.3")
+@hydra.main(config_path="Heimdall/config", config_name="config", version_base="1.3")
 def main(config):
     trainer = setup_trainer(config)
     if trainer is not None:
