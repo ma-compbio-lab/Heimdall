b166380f71cb40e92b2b509b7c818a7b03f43ab2
Shahul Alam <alam.shahul@gmail.com>
2025-10-22 20:58:22 -0400

    Fix for accelerator device.

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from Heimdall.trainer import setup_trainer
 
 @hydra.main(config_path="Heimdall/config", config_name="config", version_base="1.3")
 def main(config):
-    trainer = setup_trainer(config)
+    trainer = setup_trainer(config, cpu=(config.trainer.accelerator == "cpu"))
     if trainer is not None:
         trainer.fit()
 
